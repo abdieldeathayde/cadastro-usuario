46e816395021b99c587d990b557c978e625f581a
Abdiel Athayde <abdieldeathayde67@gmail.com>
2025-07-27 20:26:16 -0300

    adicionando desafio cadastro de usuarios

---
+++ src/main/java/com/javanauta/cadastro_usuario/infraestructure/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package com.javanauta.cadastro_usuario.infraestructure.repository;
+
+import com.javanauta.cadastro_usuario.infraestructure.entities.Usuario;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
+}
